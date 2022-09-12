fc4354a62ba356f678302171e577eb986911511c
AstroAntics <vucinicm@asbindia.org>
2022-09-12 14:25:46 +0300

    Everything for today

---
+++ static/week2/day1/main.js
@@ -0,0 +1,30 @@
+///
+
+let globalAddress = {
+  addressNumber: "101 ",
+  addressStreet: "Anywhere St. ",
+  country: "New York, NY ",
+};
+
+var result =
+  globalAddress.addressNumber +
+  globalAddress.addressStreet +
+  globalAddress.country;
+console.log(result);
+
+///
+
+const birthYear = 2004;
+const futureYear = 2038;
+const diff = 2038 - 2004;
+console.log(`I will be ${diff} years old in ${futureYear}`);
+
+///
+
+const account = {
+  username: "vucinicm",
+  password: "root",
+};
+
+const database = [account];
+const newsfeed = [account, database];
